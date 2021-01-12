1a4bae683835ed60843eee39881b678cf4037d79
Tomas Haugland Spangelo <tomas@spangelo.com>
2021-01-12 17:20:30 +0100

    Initial setup of project

---
+++ Project 1/main.py
@@ -0,0 +1,3 @@
+if __name__ == "__main__":
+    # Code to run
+    # May want to move this to network.py
---
+++ Project 1/network.py
@@ -0,0 +1,80 @@
+
+
+# Class for Layer objects
+# TODO: initial weight ranges
+class Layer:
+
+    def __init__(self, neurons, activation, weight_ranges):
+
+        self.activation = None
+
+        if activation == 'sigmoid':
+            self.activation = self._sigmoid
+
+        if activation == 'tanh':
+            self.activation = self._tanh
+
+        if activation == 'reulu':
+            self.activation = self._reulu
+
+        if activation == 'linear':
+            self.activation = self._linear
+
+        if activation == 'softmax':
+            self.activation = self._softmax
+
+        self.neurons = neurons
+        self.weight_ranges = weight_ranges
+
+    def forward_pass(self):
+        return
+
+    def backward_pass(self):
+        return
+
+    def _sigmoid(self):
+        return
+
+    def _tanh(self):
+        return
+
+    def _reulu(self):
+        return
+
+    def _linear(self):
+        return
+
+    def _softmax(self):
+        return
+
+
+
+
+# Class for Network objects
+class Network:
+
+    def __init__(self, layers, loss, regularization, reg_rate):
+        self.layers = layers
+        self.loss = loss  # TODO: Need to make internal functions for this (i.e. _mse)
+        self.regularization = regularization  # TODO: Need to incorporate this in loss function
+
+    def forward_pass(self):
+        # TODO
+
+    def backward_pass(self):
+        # TODO
+
+    def _mse(self):
+        return
+
+    def _cross_entropy(self):
+        return
+
+    def _l_one(self):
+        return
+
+    def _l_two(self):
+        return
+
+
+
