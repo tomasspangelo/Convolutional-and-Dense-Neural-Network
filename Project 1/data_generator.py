51fb1c66b0d13c1e3fcd2e6d541ef6503bb27af9
Tomas Haugland Spangelo <tomas@spangelo.com>
2021-01-31 17:54:42 +0100

    Added data generator skeleton

---
+++ Project 1/data_generator.py
@@ -0,0 +1,44 @@
+class DataGenerator:
+    def __init__(self, n, size_range, noise, train_size, test_size, val_size):
+        self.n = n
+        self.size_range = size_range
+        self.noise = noise
+        self.train_size = train_size
+        self.test_size = test_size
+        self.val_size = val_size
+
+    def generate_data(self, size):
+        return DataSet()
+
+
+class DataSet:
+    def __init__(self):
+        self.data = []
+
+    def add(self, image):
+        self.data.append(image)
+
+    def flatten(self):
+
+        flat_data = []
+        labels = []
+
+        for image in self.data:
+            flat_data.append(image.flatten())
+            labels.append(image.get_label())
+
+        return flat_data, labels
+
+
+class Image:
+    def __init__(self, flat, label):
+        self.label = label
+        self.flat = flat
+
+
+    def flatten(self):
+        return self.flat
+
+    def get_label(self):
+        return self.label
+
